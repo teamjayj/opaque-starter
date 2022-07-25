a84afeb68ef28b0c1fd9850705c1bf9957393597
jg-rivera <rivera.jga10@gmail.com>
2022-07-25 13:30:25 +0000

    feat: improved tests

--- packages/opaque-nthparty-driver/src/client/opaque-client-v2.spec.ts
+++ packages/opaque-nthparty-driver/src/client/opaque-client-v2.spec.ts
@@ -3,25 +3,26 @@ import { OpaqueNthPartyUtilV2 } from '../common/opaque-util-v2';
 import OPRF from 'oprf';
 import { OpaqueNthPartyProtocolClientV2 } from './opaque-client-v2';
 import { OpaqueNthPartyProtocolServerV2 } from '../server';
-import { hexStringToUint8Array } from '../common/util';
+import { hexStringToUint8Array } from '../common/hex-util';
 
 describe('OPAQUE Client V2', () => {
     let client: OpaqueNthPartyProtocolClientV2;
     let server: OpaqueNthPartyProtocolServerV2;
+    let util: OpaqueNthPartyUtilV2;
 
     const userId = 'bob';
     const plaintextPassword = 'password';
 
-    beforeAll(async () => {
+    beforeEach(async () => {
         const oprf = new OPRF();
         await oprf.ready;
 
-        const util = new OpaqueNthPartyUtilV2(sodium, oprf);
+        util = new OpaqueNthPartyUtilV2(sodium, oprf);
         client = new OpaqueNthPartyProtocolClientV2(sodium, util);
         server = new OpaqueNthPartyProtocolServerV2(sodium, util);
     });
 
-    beforeEach(() => {
+    afterEach(() => {
         client.clearStore();
         server.clearStore();
     });
@@ -46,18 +47,17 @@ describe('OPAQUE Client V2', () => {
             plaintextPassword,
             userId
         );
-
         const hashedPassword = hexStringToUint8Array(request.hashedPassword);
+        const userRecord = await server.serverRegister(userId, hashedPassword);
 
-        const record = await server.serverRegister(userId, hashedPassword);
-        expect(record).toBeTruthy();
-        expect(record.userId).toEqual(userId);
-        expect(record.pepper).toBeDefined();
-        expect(record.pepper.envelope).toBeDefined();
+        expect(userRecord).toBeTruthy();
+        expect(userRecord.userId).toEqual(userId);
+        expect(userRecord.pepper).toBeDefined();
+        expect(userRecord.pepper.envelope).toBeDefined();
     });
 
     it('should create credential request', async () => {
-        const request = await client.clientBeginAuthenticate('password');
+        const request = await client.clientBeginAuthenticate(plaintextPassword);
 
         expect(client.get('r')).toBeDefined();
         expect(client.get('xu')).toBeDefined();
@@ -67,5 +67,83 @@ describe('OPAQUE Client V2', () => {
         expect(request).toBeTruthy();
         expect(request.alpha).toBeDefined();
         expect(request.Xu).toBeDefined();
+    });
+
+    it('should successfully initialize auth with server', async () => {
+        const registrationRequest = await client.createRegistrationRequest(
+            plaintextPassword,
+            userId
+        );
+        const hashedPassword = hexStringToUint8Array(
+            registrationRequest.hashedPassword
+        );
+
+        // 1: Client -> Server
+
+        const userRecord = await server.serverRegister(userId, hashedPassword);
+
+        // 1: Client <- Server (registration success or fail)
+
+        const credentialRequest = await client.clientBeginAuthenticate(
+            plaintextPassword
+        );
+
+        // 2: Client -> Server
+
+        const credentialResponse = await server.serverBeginAuthenticate(
+            hexStringToUint8Array(credentialRequest.alpha),
+            hexStringToUint8Array(credentialRequest.Xu),
+            userRecord.pepper
+        );
+
+        // 2: Client <- Server (credential response or fail)
+
+        expect(server.get('SK')).toBeDefined();
+        expect(server.get('Au')).toBeDefined();
+
+        expect(credentialResponse).toBeTruthy();
+        expect(credentialResponse.beta).toBeDefined();
+        expect(credentialResponse.Xs).toBeDefined();
+        expect(credentialResponse.envelope).toBeDefined();
+        expect(credentialResponse.As).toBeDefined();
+    });
+
+    it('should successfully perform key exchange with server', async () => {
+        const registrationRequest = await client.createRegistrationRequest(
+            plaintextPassword,
+            userId
+        );
+        const hashedPassword = hexStringToUint8Array(
+            registrationRequest.hashedPassword
+        );
+
+        // 1: Client -> Server
+
+        const userRecord = await server.serverRegister(userId, hashedPassword);
+
+        // 1: Client <- Server (registration success or fail)
+
+        const credentialRequest = await client.clientBeginAuthenticate(
+            plaintextPassword
+        );
+
+        // 2: Client -> Server
+
+        const credentialResponse = await server.serverBeginAuthenticate(
+            hexStringToUint8Array(credentialRequest.alpha),
+            hexStringToUint8Array(credentialRequest.Xu),
+            userRecord.pepper
+        );
+
+        // 2: Client <- Server (credential response or fail)
+
+        const request = await client.clientKeyExchangeAuthenticate(
+            hexStringToUint8Array(credentialResponse.beta),
+            hexStringToUint8Array(credentialResponse.Xs),
+            hexStringToUint8Array(credentialResponse.As),
+            util.stringEnvelopeToEnvelope(credentialResponse.envelope)
+        );
+
+        expect(request).toBeTruthy();
     });
 });
