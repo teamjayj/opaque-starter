495e15f8b9022e2aa31711af1530da24080b5040
jg-rivera <rivera.jga10@gmail.com>
2022-07-25 13:30:07 +0000

    feat: changed to map implementation

--- packages/opaque-nthparty-driver/src/common/opaque-v2.ts
+++ packages/opaque-nthparty-driver/src/common/opaque-v2.ts
@@ -2,24 +2,30 @@ import { OpaqueNthPartyUtilV2 } from './opaque-util-v2';
 import Sodium from 'libsodium-wrappers-sumo';
 
 export class OpaqueNthPartyProtocolV2 {
-    protected store: Record<string, Uint8Array>;
+    protected store: Map<string, Uint8Array>;
 
     constructor(
         protected sodium: typeof Sodium,
         protected util: OpaqueNthPartyUtilV2
     ) {
-        this.store = {};
+        this.store = new Map();
     }
 
     protected set(key: string, value: Uint8Array): void {
-        this.store[key] = value;
+        this.store.set(key, value);
     }
 
     public get(key: string): Uint8Array {
-        return this.store[key];
+        const value = this.store.get(key);
+
+        if (value == null) {
+            throw new Error(`${key} is undefined in store`);
+        }
+
+        return value;
     }
 
     public clearStore(): void {
-        this.store = {};
+        this.store.clear();
     }
 }
