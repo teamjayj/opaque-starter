b8ad8a685b8175e07a0437dc9c66399d411755c3
jg-rivera <rivera.jga10@gmail.com>
2022-07-25 11:37:33 +0000

    feat: added memory store

--- packages/opaque-nthparty-driver/src/common/opaque-v2.ts
+++ packages/opaque-nthparty-driver/src/common/opaque-v2.ts
@@ -2,7 +2,7 @@ import { OpaqueNthPartyUtilV2 } from './opaque-util-v2';
 import Sodium from 'libsodium-wrappers-sumo';
 
 export class OpaqueNthPartyProtocolV2 {
-    protected store: Record<string, string>;
+    protected store: Record<string, Uint8Array>;
 
     constructor(
         protected sodium: typeof Sodium,
@@ -11,11 +11,11 @@ export class OpaqueNthPartyProtocolV2 {
         this.store = {};
     }
 
-    protected set(key: string, value: string): void {
+    protected set(key: string, value: Uint8Array): void {
         this.store[key] = value;
     }
 
-    protected get(key: string): string {
+    protected get(key: string): Uint8Array {
         return this.store[key];
     }
 }
