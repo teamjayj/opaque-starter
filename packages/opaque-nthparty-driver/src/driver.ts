9e1416bff8a157559b92e4f0523b11fb09627132
jg-rivera <rivera.jga10@gmail.com>
2022-07-25 07:04:38 +0000

    feat: updated driver initialization

--- packages/opaque-nthparty-driver/src/driver.ts
+++ packages/opaque-nthparty-driver/src/driver.ts
@@ -1,28 +1,33 @@
-import * as opaque from '@nthparty/opaque';
 import { PakeClientDriver, PakeServerDriver } from '@jayj/pake';
-import { OpaqueNthPartyProtocol } from './types';
-import { DummySocket } from './dummy-socket';
+import { OpaqueNthPartyProtocolV2 } from './opaque-v2';
+import Sodium from 'libsodium-wrappers-sumo';
+import { OpaqueNthPartyUtilV2 } from './opaque-util-v2';
+import OPRF from 'oprf';
 
 export class OpaqueNthPartyDriver
     implements PakeClientDriver, PakeServerDriver
 {
-    private protocol: OpaqueNthPartyProtocol;
+    private protocol: OpaqueNthPartyProtocolV2 | undefined;
 
-    constructor(private socket: DummySocket) {}
+    constructor(private sodium: typeof Sodium) {}
 
     async initialize(): Promise<void> {
-        this.protocol = await opaque(this.socket);
+        const oprf = new OPRF();
+        await oprf.ready;
+
+        const utilV2 = new OpaqueNthPartyUtilV2(this.sodium, oprf);
+        this.protocol = new OpaqueNthPartyProtocolV2(this.sodium, utilV2);
     }
 
     async registerAsClient(password: string, userId: string): Promise<void> {
-        this.protocol.clientRegister(password, userId);
+        this.protocol?.clientRegister(password, userId);
     }
 
     async authenticateAsClient(
         password: string,
         userId: string
     ): Promise<void> {
-        const token = await this.protocol.clientAuthenticate(password, userId);
+        const token = await this.protocol?.clientAuthenticate(password, userId);
     }
 
     async registerAsServer(): Promise<void> {}
