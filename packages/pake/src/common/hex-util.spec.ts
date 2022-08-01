9f099eefaa0869f3135acd0d992bd0f1a00d43d8
jg-rivera <rivera.jga10@gmail.com>
2022-08-01 08:55:42 +0000

    chore: moved hex util to pake common

---
+++ packages/pake/src/common/hex-util.spec.ts
@@ -0,0 +1,11 @@
+import { hexStringToUint8Array } from '.';
+
+describe('Hex util', () => {
+    it('should convert hex string to uint8array', async () => {
+        const hexPassword = Buffer.from('password', 'utf8').toString('hex');
+
+        expect(hexStringToUint8Array(hexPassword)).toEqual(
+            Uint8Array.from(Buffer.from(hexPassword, 'hex'))
+        );
+    });
+});
