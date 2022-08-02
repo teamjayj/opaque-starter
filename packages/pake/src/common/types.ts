9c1b3f0cebc5a090132c7b3800d9f9a998d7544d
jg-rivera <rivera.jga10@gmail.com>
2022-08-02 11:58:57 +0000

    feat: added response types

--- packages/pake/src/common/types.ts
+++ packages/pake/src/common/types.ts
@@ -9,3 +9,13 @@ export type HexString = Opaque<'HexString', string>;
  * Types for serial data. Currently only supports hex strings.
  */
 export type SerialData = HexString;
+
+export type ClientAuthFinishResponse = {
+    clientRequest: SerialData;
+    sessionKey: SerialData;
+};
+
+export type ServerAuthInitResponse = {
+    serverResponse: SerialData;
+    expectedAuthResult: SerialData;
+};
