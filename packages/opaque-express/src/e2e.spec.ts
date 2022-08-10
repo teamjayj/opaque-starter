e1d38ea47c8947949133fe32debbeb7af8185b1f
jg-rivera <rivera.jga10@gmail.com>
2022-08-10 06:15:18 +0000

    feat: migrate express package to use vitest

--- packages/opaque-express/src/e2e.spec.ts
+++ packages/opaque-express/src/e2e.spec.ts
@@ -1,4 +1,6 @@
+import { describe, it } from 'vitest';
 import express, { Application, Express } from 'express';
+
 describe('End-to-end test', () => {
     it('should create an instance', () => {
         const app: Application = express();
