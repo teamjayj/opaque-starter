1eeb740c7d3ef8acc213018912f896563d524a6c
jg-rivera <rivera.jga10@gmail.com>
2022-08-10 06:00:54 +0000

    fix: remove type reference

--- packages/pake/src/common/hex-util.spec.ts
+++ packages/pake/src/common/hex-util.spec.ts
@@ -1,10 +1,8 @@
-import { hexStringToUint8Array, HexString } from '.';
+import { hexStringToUint8Array } from '.';
 
 describe('Hex util', () => {
     it('should convert hex string to uint8array', async () => {
-        const hexPassword = Buffer.from('password', 'utf8').toString(
-            'hex'
-        ) as HexString;
+        const hexPassword = Buffer.from('password', 'utf8').toString('hex');
 
         expect(hexStringToUint8Array(hexPassword)).toEqual(
             Uint8Array.from(Buffer.from(hexPassword, 'hex'))
