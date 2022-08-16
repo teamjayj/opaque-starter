756c0f06dd8a91e8fe03278bc8bbe6518e7b2ce3
jg-rivera <rivera.jga10@gmail.com>
2022-08-16 07:41:49 +0000

    docs: add jsdoc to store

--- packages/pake/src/common/store/api/credential.store.ts
+++ packages/pake/src/common/store/api/credential.store.ts
@@ -1,5 +1,18 @@
 export interface OpaqueCredentialStore {
+    /**
+     * Persists credential data to store.
+     *
+     * @param credentialId - credential identifier
+     * @param credentialFile - credential file from server's `registerFinish` step.
+     */
     store(credentialId: string, credentialFile: Uint8Array): Promise<void>;
 
+    /**
+     * Retrieves credential file from an ID.
+     *
+     * @param credentialId - credential identifier
+     *
+     * @returns credential file of client
+     */
     get(credentialId: string): Promise<Uint8Array>;
 }
--- packages/pake/src/common/store/api/session.store.ts
+++ packages/pake/src/common/store/api/session.store.ts
@@ -1,9 +1,26 @@
+/**
+ * Session store should be time-sensitive.
+ */
 export interface OpaqueSessionStore {
+    /**
+     * Persists expected auth result to store.
+     *
+     * @param sessionId - session identifier
+     * @param expectedAuthResult - expected auth data result from server's `authInit` step
+     * @param ttl - time-to-live in milliseconds of session data
+     */
     store(
         sessionId: string,
         expectedAuthResult: Uint8Array,
         ttl?: number
     ): Promise<void>;
 
+    /**
+     * Retrieves session data from an ID.
+     *
+     * @param sessionId - session identifier
+     *
+     * @returns expected auth data of session
+     */
     get(sessionId: string): Promise<Uint8Array>;
 }
