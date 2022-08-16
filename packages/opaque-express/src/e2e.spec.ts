a15ddf9165b74e699a6b8d0ffc684de997162e44
jg-rivera <rivera.jga10@gmail.com>
2022-08-16 06:43:19 +0000

    test: use supertest in e2e testing

--- packages/opaque-express/src/e2e.spec.ts
+++ packages/opaque-express/src/e2e.spec.ts
@@ -1,8 +1,23 @@
-import { describe, it } from 'vitest';
-import express, { Application, Express } from 'express';
+import { beforeAll, describe, expect, it } from 'vitest';
+import express, { Application } from 'express';
+import request from 'supertest';
 
 describe('End-to-end test', () => {
-    it('should create an instance', () => {
-        const app: Application = express();
+    let app: Application;
+
+    beforeAll(() => {
+        app = express();
+
+        app.get('/', (req, res) => {
+            res.status(200).json({ message: 'hi' });
+        });
+    });
+
+    it('GET / should return hi message', async () => {
+        const response = await request(app).get('');
+        expect(response.statusCode).toEqual(200);
+        expect(response.body).toEqual({
+            message: 'hi',
+        });
     });
 });
